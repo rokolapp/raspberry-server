0bf678bd8da6a13ab9cbe35423cbcefade868123
Alejandro Cuenca Estrada <alexcuenca4@gmail.com>
2015-04-14 21:12:36 -0500

    Playing song and queue added

--- playsong.js
+++ playsong.js
@@ -1,38 +1,53 @@
-var http = require('http');
-var url = require('url');
+var http 	= require('http'),
+	url 	= require('url'),
+	lame 	= require('lame'),
+	Speaker = require('speaker'),
+	Spotify = require('spotify-web');
 
-var lame = require('lame'),
-		Speaker = require('speaker'),
-		Spotify = require('spotify-web');
-
+/*----------------SERVER CONFIG----------------*/ 
 const PORT = 8080;
+var server = http.createServer(handleRequest);
+
+server.listen(PORT, function(){
+	console.log('Servidor encendido');
+});
+
+/*----------------SERVER Controller----------------*/
+var playlist = [];
 
 function handleRequest(req, res){
+
+	console.log(req.url)
+
 	if(req.url == '/favicon.ico')
 		res.end();
+
+
+	if(req.url == '/playlist'){
+		var jsonObject = {};
+		jsonObject.playlist = playlist;
+		res.write(JSON.stringify(jsonObject));
+		res.end();
+	}
+
 	else{
-		console.log(req.url)
+		console.log('lol')
 		var paresedUrl = url.parse(req.url, true);
 		var queryAsObject = paresedUrl.query; 
-
-		console.log(queryAsObject.q);
-		playsong(queryAsObject.q)
+		
+		playlist.push(queryAsObject.q);
+		
+		if (playlist.length == 1)
+			playsong()
 
 		res.end('Playing song');
 	}
 }
 
-var server = http.createServer(handleRequest);
 
-server.listen(PORT, function(){
-	console.log('Server ready');
-});
-
-function playsong (track_uri)
+function playsong ()
 {
-	
-
-	var url = track_uri || 'spotify:track:1ZBAee0xUblF4zhfefY0W1';
+	//var url = track_uri || 'spotify:track:1ZBAee0xUblF4zhfefY0W1';
 
 	var username = '12156614669',
 		password = 'Getinhalo4';
@@ -40,6 +55,8 @@ function playsong (track_uri)
 	Spotify.login(username,password, function(err,spotify){
 		if (err) throw err;
 		//Get a track instance
+		var url = playlist[0];
+
 		spotify.get(url, function(err,track){
 			if (err) throw err;
 			console.log('Playing %s - %s', track.artist[0].name, track.name);
@@ -50,7 +67,10 @@ function playsong (track_uri)
 			.pipe(new Speaker())
 			.on('finish', function(){
 				spotify.disconnect();
-				console.log('bye')
+				console.log('end song')
+				playlist.shift();				
+				if(playlist.length > 0)
+					playsong();
 			});
 
 		});
