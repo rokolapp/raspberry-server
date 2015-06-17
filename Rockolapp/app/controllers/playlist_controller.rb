7c5fef3944c00d847521af3e615a62b3a7090814
WilfredoIvanhoe <jordan.965@hotmail.com>
2015-06-17 11:03:48 -0500

    Worked on playlists

--- Rockolapp/app/controllers/playlist_controller.rb
+++ Rockolapp/app/controllers/playlist_controller.rb
@@ -1,9 +1,76 @@
+require 'net/http'
+require 'uri'
 class PlaylistController < ApplicationController
 
 	def new
-
+		req_ip = request.remote_ip
+		songs = Playlist.all
+		songs.each{|song| redirect_to '/playlist' if req_ip == song.ip}
 	end
+	def search
+		q = params[:q]
+		limit = params[:limit]
+		url = "https://api.spotify.com/v1/search"
+		params = "?q=#{q}&type=track&limit=#{limit}"
+		uri = URI(URI.encode(url+params))
+		res = Net::HTTP.get(uri)
+		json = JSON.parse(res)
+		route = File.join(Rails.root, 'config', 'list-mode.xlu')
+		list = File.foreach(route).first.chomp
 
+		if list.eql? "whitelist" and json
+
+			tracks = json["tracks"]
+			items = json["tracks"]["items"]
+			puts "Longitud de items: #{items.length}"
+			items.delete_if do |item|
+				album = Album.exists?(uri: item["album"]["uri"])
+				puts "Album: #{album}"
+				artists = item["artists"]
+				artist = false
+				artists.each{|art| artist = Artist.exists?(uri: art["uri"])}
+				puts "Artist: #{artist}"
+				track_uri = Track.exists?(uri: item["uri"])
+				puts "Track #{track_uri}"
+				if item and (album or artist or track_uri)
+					puts "true"
+					false
+				else
+					puts "false"
+					true
+				end
+			end
+			render json: json.to_json
+		elsif list.eql? "blacklist" and json
+
+			tracks = json["tracks"]
+			items = json["tracks"]["items"]
+			puts "Longitud de items: #{items.length}"
+			items.delete_if do |item|
+				album = Album.exists?(uri: item["album"]["uri"])
+				puts "Album: #{album}"
+				artists = item["artists"]
+				artist = false
+				artists.each{|art| artist = Artist.exists?(uri: art["uri"])}
+				puts "Artist: #{artist}"
+				track_uri = Track.exists?(uri: item["uri"])
+				puts "Track #{track_uri}"
+				if item and (album or artist or track_uri)
+					puts "true"
+					true
+				else
+					puts "false"
+					false
+				end
+			end
+			render json: json.to_json
+		elsif list.eql? "freeforall" and json
+			render json: json.to_json
+		else
+			render json: '{error: "Hubo problemas"}'
+		end
+		
+	end
 	def save
 		puts "\n\n LA IP ES ESTA WEY: #{request.remote_ip} \n\n"
 		redirect_to '/playlist/new'
--- Rockolapp/app/models/playlist.rb
+++ Rockolapp/app/models/playlist.rb
@@ -1,4 +1,4 @@
 class Playlist < ActiveRecord::Base
-	validates: :uri, uniqueness: {message: " : Esta cancion ya se ha pedido, por favor elija otra"}
-	validates: :ip, uniqueness: {message: " : Solo puede pedir una canción a la vez, por favor espere a que se reproduzca la suya"}
+	validates :uri, uniqueness: {message: " : Esta cancion ya se ha pedido, por favor elija otra"}
+	validates :ip, uniqueness: {message: " : Solo puede pedir una canción a la vez, por favor espere a que se reproduzca la suya"}
 end
